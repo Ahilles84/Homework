862bbccbe9eb79bd286461ad49bae24b59161803
Oleg Kuleshov <7100570@gmail.com>
2022-09-19 16:11:47 +0300

    solved

--- task_one/src/task_4/UniqueWords.java
+++ task_one/src/task_4/UniqueWords.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class UniqueWords {
     public static int uniqueWordsCounter (String sourceText) {
-        String[] words = sourceText.split("[.,?!-]? +");
+        String[] words = sourceText.split(" ?[.,?!-]? +");
         int uniqueCounter = 0;
         for (String word : words) {
             int hitCounter = 0;
@@ -21,7 +21,10 @@ public class UniqueWords {
     }
     public static void main (String[] args){
         String text = "Периодически в новостях всплывают вакансии, о которых только можно мечтать. В реальности некоторые из них оказываются маркетинговым ходом - компаниям нужно ваше внимание, а ваша трудовая книжка - нет. Попробуйте угадать, какие из этих вакансий существовали в действительности. ";
-        System.out.println(Arrays.toString(text.split("[,.!?-]? +")));
+        String engText = "Listen to the news from today and read the text at the same time. Listen to the news from today without reading the text. ";
+        System.out.println(Arrays.toString(text.split(" ?[,.!?-]? +")));
+        System.out.println(Arrays.toString(engText.split(" ?[,.!?-]? +")));
         System.out.println(uniqueWordsCounter(text));
+        System.out.println(uniqueWordsCounter(engText));
     }
 }
