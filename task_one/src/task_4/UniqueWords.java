6112036f7459e5338fae63f630bfe99b79c51cb9
Oleg Kuleshov <7100570@gmail.com>
2022-09-25 10:46:25 +0300

    added new method wordsCollector.

--- task_one/src/task_4/UniqueWords.java
+++ task_one/src/task_4/UniqueWords.java
@@ -19,12 +19,30 @@ public class UniqueWords {
         }
         return uniqueCounter;
     }
+    public static int wordsCollector(String sourceText) {
+        String[] words = sourceText.split(" ?[.,?!-]? +");
+        for (int i = 1; i < words.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (words[i].equalsIgnoreCase(words[j])) {
+                    words[i] = null;
+                    break;
+                }
+            }
+        }
+        int uniqueWords = 0;
+        for (String word:words) {
+            if(word != null) {
+                uniqueWords++;
+            }
+        }
+        return uniqueWords;
+    }
     public static void main (String[] args){
+        String song = "В лесу родилась Ёлочка, в лесу она росла! ";
         String text = "Периодически в новостях всплывают вакансии, о которых только можно мечтать. В реальности некоторые из них оказываются маркетинговым ходом - компаниям нужно ваше внимание, а ваша трудовая книжка - нет. Попробуйте угадать, какие из этих вакансий существовали в действительности. ";
         String engText = "Listen to the news from today and read the text at the same time. Listen to the news from today without reading the text. ";
-        System.out.println(Arrays.toString(text.split(" ?[,.!?-]? +")));
-        System.out.println(Arrays.toString(engText.split(" ?[,.!?-]? +")));
-        System.out.println(uniqueWordsCounter(text));
-        System.out.println(uniqueWordsCounter(engText));
+        System.out.println(wordsCollector(song));
+        System.out.println(wordsCollector(text));
+        System.out.println(wordsCollector(engText));
     }
 }
