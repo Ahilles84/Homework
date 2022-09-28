8934a3fa0e9860b0aa5040674ceec69e5270c1b7
Oleg Kuleshov <7100570@gmail.com>
2022-09-29 00:01:03 +0300

    same type

---
+++ task_one/src/task_6/Main.java
@@ -0,0 +1,14 @@
+package task_6;
+
+public class Main {
+    public static void main (String[] args){
+        Pair<String> pair = new Pair<>("First", "Second");
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getLast());
+        System.out.println(pair.replaceFirst("One"));
+        System.out.println(pair.replaceLast("Two"));
+        System.out.println(pair.toString());
+        pair.swap(pair);
+        System.out.println(pair.toString());
+    }
+}
---
+++ task_one/src/task_6/Pair.java
@@ -0,0 +1,38 @@
+package task_6;
+
+public class Pair <T>{
+    private T first;
+    private T last;
+
+    Pair(T first, T last){
+        this.first = first;
+        this.last = last;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+    public T getLast() {
+        return last;
+    }
+    public T replaceFirst(T newValue){
+        first = newValue;
+        return first;
+    }
+    public T replaceLast(T newValue){
+        last = newValue;
+        return last;
+    }
+    public Pair swap(Pair pair){
+        T temp = first;
+        first = last;
+        last = temp;
+        return pair;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + first +", " + last +"]";
+    }
+}
+
