0f07b89f1b87f8f5f2c6a39a5aab4f10b3614c06
Oleg Kuleshov <7100570@gmail.com>
2022-09-30 15:20:05 +0300

    solvation variant

--- task_one/src/task_7/Mapper.java
+++ task_one/src/task_7/Mapper.java
@@ -1,20 +1,19 @@
 package task_7;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Mapper <K>{
     public static <K> Map<K, Integer> arrayToMap(K[] ks) {
         Map<K, Integer> map = new HashMap<>();
-        for (int i = 0; i < ks.length; i++) {
+        for (K k : ks) {
             int value = 0;
-            for(int j = 0; j < ks.length; j++){
-                if (ks[i].equals(ks[j])){
+            for (K item : ks) {
+                if (k.equals(item)) {
                     value++;
                 }
             }
-            map.put(ks[i],value);
+            map.put(k, value);
         }
         return map;
     }
@@ -22,7 +21,10 @@ public class Mapper <K>{
     public static void main(String[] args){
         String text = "Listen to the news from today and read the text at the same time. Listen to the news from today without reading the text. ";
         String[] words = text.split(" ?[.,?!-]? +");
-        Map<String, Integer> calendar = arrayToMap(words);
-        System.out.println(calendar.toString());
+        Map<String, Integer> myMapOne = arrayToMap(words);
+        System.out.println(myMapOne);
+        Double[] numbers = new Double[]{1.2, 15.0, 24.55, 0.554, 1.3, 4.502, 8521.2554, 1.2, 0.554};
+        Map<Double, Integer> myMapTwo = arrayToMap(numbers);
+        System.out.println(myMapTwo);
     }
 }
