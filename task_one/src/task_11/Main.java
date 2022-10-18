0c13c4a09c18218e8f4c6d5f264b005fa6f66b22
Oleg Kuleshov <7100570@gmail.com>
2022-10-18 17:07:31 +0300

    Streams are cool

---
+++ task_one/src/task_11/Main.java
@@ -0,0 +1,28 @@
+package task_11;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args){
+        String str1 = "a1";
+        String str2 = "a2";
+        String str3 = "a3";
+        String str4 = "b1";
+        String str5 = "b3";
+        String str6 = "c2";
+        String str7 = "c1";
+        String str8 = "c5";
+        List<String> myList = List.of(str1,str2,str3,str4,str5,str6,str7,str8);
+        String newList = myList.stream().filter(str -> !str.contains("3")).
+                //вот тут должна быть сортировка по цифре по возрастанию
+                sorted(Comparator.reverseOrder()).
+                skip(1).
+                map(str -> str.replace("1", "9")).
+                map(String::toUpperCase).
+                collect(Collectors.joining("; "));
+        System.out.println(newList);
+
+    }
+}
