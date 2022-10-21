c48d0a3620a8e1ad9ca41de99572bae03f9145e8
Oleg Kuleshov <7100570@gmail.com>
2022-10-21 17:26:25 +0300

    MyGame

--- task_one/src/task_11/Main.java
+++ task_one/src/task_11/Main.java
@@ -1,5 +1,6 @@
 package task_11;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,15 +16,15 @@ public class Main {
         String str7 = "c1";
         String str8 = "c5";
         List<String> myList = List.of(str1,str2,str3,str4,str5,str6,str7,str8);
-        String newList = myList.stream().
+        List<String> newList = myList.stream().
                 filter(str -> !str.contains("3")).
                 sorted(Comparator.comparingInt(str->str.charAt(1))).
                 sorted(Comparator.reverseOrder()).
                 skip(1).
                 map(str -> str.replace("1", "9")).
                 map(String::toUpperCase).
-                collect(Collectors.joining("; "));
-        System.out.println(newList);
-
+                collect(Collectors.toList());
+        String output = String.join("; ", newList);
+        System.out.println(output);
     }
 }
