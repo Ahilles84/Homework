b46e28d04691ba16bbe9dbc8513fd6756d763541
Oleg Kuleshov <7100570@gmail.com>
2022-11-28 17:09:35 +0300

    refactoring

--- task_one/src/task_3/PrimeArrayCreator.java
+++ task_one/src/task_3/PrimeArrayCreator.java
@@ -3,32 +3,26 @@ package task_3;
 import java.util.Arrays;
 
 public class PrimeArrayCreator {
-    public static int[] PrimeArray(int[] source) {
-        int[] temp = new int[source.length];
-        int k = 0;
-        for (int i = source[source.length - 1]; i > 0; i--) {
-            int countDividers = 0;
-            for (int j = i; j > 0; j--) {
-                if (i % j == 0) {
-                    countDividers++;
-                    if (countDividers > 2) {
-                        break;
-                    }
-                }
-            }
-            if (countDividers <= 2) {
-                temp[k] = i;
-                k++;
-            }
+  public static int[] PrimeArray(int[] source) {
+    int[] temp = new int[source.length];
+    int primeIndex = 0;
+    for (int i = source[source.length - 1]; i > 0; i--) {
+      int countDividers = 0;
+      for (int j = i; j > 0; j--) {
+        if (i % j == 0) {
+          countDividers++;
+          if (countDividers > 2) {
+            break;
+          }
         }
-        int countPrimeNumbers = 0;
-        for (int val : temp) {
-            if (val > 1) {
-                countPrimeNumbers++;
-            }
-        }
-        int[] primeArray = Arrays.copyOf(temp, countPrimeNumbers);
-        Arrays.sort(primeArray);
-        return primeArray;
+      }
+      if (countDividers <= 2) {
+        temp[primeIndex] = i;
+        primeIndex++;
+      }
     }
+    int[] primeArray = Arrays.copyOf(temp, primeIndex);
+    Arrays.sort(primeArray);
+    return primeArray;
+  }
 }
