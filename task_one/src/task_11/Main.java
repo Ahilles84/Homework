b3dbd4647b59dd0b8d8d44824ada56f12159c2a5
Oleg Kuleshov <7100570@gmail.com>
2022-11-28 18:00:33 +0300

    refactoring

--- task_one/src/task_11/Main.java
+++ task_one/src/task_11/Main.java
@@ -5,25 +5,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
-        String str1 = "a1";
-        String str2 = "a2";
-        String str3 = "a3";
-        String str4 = "b1";
-        String str5 = "b3";
-        String str6 = "c2";
-        String str7 = "c1";
-        String str8 = "c5";
-        List<String> myList = List.of(str1, str2, str3, str4, str5, str6, str7, str8);
-        List<String> newList = myList.stream().
-                filter(str -> !str.contains("3")).
-                sorted(Comparator.comparingInt(str -> str.charAt(1))).
-                sorted(((o1, o2) -> o2.charAt(0) - o1.charAt(0))).
-                skip(1).
-                map(str -> str.replace("1", "9")).
-                map(String::toUpperCase).
-                collect(Collectors.toList());
-        String output = String.join("; ", newList);
-        System.out.println(output);
-    }
+  public static void main(String[] args) {
+    String str1 = "a1";
+    String str2 = "a2";
+    String str3 = "a3";
+    String str4 = "b1";
+    String str5 = "b3";
+    String str6 = "c2";
+    String str7 = "c1";
+    String str8 = "c5";
+    List<String> myList = List.of(str1, str2, str3, str4, str5, str6, str7, str8);
+    String output =
+        myList.stream()
+            .filter(str -> !str.contains("3"))
+            .sorted(Comparator.comparingInt(str -> str.charAt(1)))
+            .sorted(((o1, o2) -> o2.charAt(0) - o1.charAt(0)))
+            .skip(1)
+            .map(str -> str.replace("1", "9"))
+            .map(String::toUpperCase)
+            .collect(Collectors.joining("; "));
+    System.out.println(output);
+  }
 }
--- task_one/src/task_9/TextStatistics.java
+++ task_one/src/task_9/TextStatistics.java
@@ -4,56 +4,57 @@ import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class TextStatistics {
-    public static int punctCounter(String text){
-        int punctuationMarks = 0;
-        Matcher matcher;
-        Pattern punctuation = Pattern.compile("\\p{Punct}");
-        String[] words = text.split("\\s");
-        for(String str: words) {
-            matcher = punctuation.matcher(str);
-            while (matcher.find()) {
-                ++punctuationMarks;
-            }
-        }
-        return punctuationMarks;
+  public static int punctCounter(String text) {
+    int punctuationMarks = 0;
+    Matcher matcher;
+    Pattern punctuation = Pattern.compile("\\p{Punct}");
+    String[] words = text.split("\\s");
+    for (String str : words) {
+      matcher = punctuation.matcher(str);
+      while (matcher.find()) {
+        ++punctuationMarks;
+      }
     }
-    public static int wordCounter(String text){
-        String[] words = text.split(" ?[.,?!-]? +");
-        return words.length;
-    }
+    return punctuationMarks;
+  }
 
-    public static void createFileFromConsoleInput(String fileName){
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))){
-            String text;
-            while (!(text = br.readLine()).equals("END")){
-                bw.write(text + "\n");
-                bw.flush();
-            }
-        } catch (IOException ex){
-            System.out.println(ex.getMessage());
-        }
-    }
-    public static String textFromFile(String fileName){
-        StringBuilder textFromFile = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))){
-            String str;
-            while ((str = br.readLine())!= null) {
-                textFromFile.append(str).append("\n");
-            }
-        } catch (IOException ex){
-            System.out.println(ex.getMessage());
-        }
-        return textFromFile.toString();
-    }
+  public static int wordCounter(String text) {
+    String[] words = text.split(" ?[.,?!-]? +");
+    return words.length;
+  }
 
-    public static void main(String[] args){
-        createFileFromConsoleInput("New_file.txt");
-        String example = textFromFile("New_file.txt");
-        System.out.println(example);
-        System.out.println("Number of words: " + wordCounter(example));
-        System.out.println("Number of punctuation marks: " + punctCounter(example));
+  public static void createFileFromConsoleInput(String fileName) {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+      String text;
+      while (!(text = br.readLine()).equals("END")) {
+        bw.write(text + "\n");
+        bw.flush();
+      }
+    } catch (IOException ex) {
+      System.out.println(ex.getMessage());
     }
+  }
+
+  public static String textFromFile(String fileName) {
+    StringBuilder textFromFile = new StringBuilder();
+    try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+      String str;
+      while ((str = br.readLine()) != null) {
+        textFromFile.append(str).append("\n");
+      }
+    } catch (IOException ex) {
+      System.out.println(ex.getMessage());
+    }
+    return textFromFile.toString();
+  }
+
+  public static void main(String[] args) {
+    createFileFromConsoleInput("New_file.txt");
+    String example = textFromFile("New_file.txt");
+    System.out.println(example);
+    System.out.println("Number of words: " + wordCounter(example));
+    System.out.println("Number of punctuation marks: " + punctCounter(example));
+  }
 }
